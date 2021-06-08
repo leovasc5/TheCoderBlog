36b1b9f651e378499dcc0766b6933fa217ed5279
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-08 19:42:50 -0300

    Estilizando...

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,20 +5,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/topBar.css">
         <link rel="stylesheet" href="/css/footer.css">
+        <link rel="stylesheet" href="/css/scrollBar.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="/img/icons/icon2.svg">
         <title>TheCoderBlog</title>
     </head>
 
     <header>
         @yield('topBar')
-        <div class="topBar" style="margin:0">
+        <div class="topBar" style="margin:0" title="TheCoderBlog">
             <a href="http://127.0.0.1:8000" class="active">
-                <img src='/img/cretch.png' width='22px'/>TheCoderBlog
+                <img src='/img/icons/icon3.svg' width='45px'/>
+                <br>
+                <label>TheCoderBlog</label>
             </a>
             <div class="lista">
                 <a href="http://127.0.0.1:8000/">Home</a>
                 <a href="http://127.0.0.1:8000/criar-partida">Artigos</a>
+                <a href="http://127.0.0.1:8000/criar-partida">Discussão</a>
+                <a href="http://127.0.0.1:8000/criar-partida">Vídeos</a>
+                <a href="http://127.0.0.1:8000/criar-partida">Projetos</a>
             </div>
         </div>
     </header>
