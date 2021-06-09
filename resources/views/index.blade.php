2a098d3d84849accde2aa5495b3e80d343d02b26
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-09 11:28:38 -0300

    Estilizando...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,20 +6,28 @@
 @section('content')
 <link rel="stylesheet" href="/css/index.css">
 
-<section>
-    <h1>Olá, eu sou Leonardo Vasconcelos, um desenvolvedor de software.<h1>
-        <img src="https://avatars.githubusercontent.com/u/70069239?v=4" class="avatar">
-        <p>
-            <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/javascript/javascript-plain.svg" height="40" width="40" style="max-width:100%;" title="JavaScript">
-            <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/php/php-plain.svg" height="40" width="40" style="max-width:100%;" title="PHP">
-            <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/laravel/laravel-plain.svg" height="40" width="40" style="max-width:100%;" title="Laravel">
-            <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/python/python-plain.svg" height="40" width="40" style="max-width:100%;" title="Python">
-            <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/mysql/mysql-original.svg" height="40" width="40" style="max-width:100%;" title="SQL">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/Vegas_Pro_15.0.png" height="40" width="40" style="max-width:100%;" title="Vegas Pro">
-            <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/photoshop/photoshop-line.svg" height="40" width="40" style="max-width:100%;" title="Photoshop">
-        </p>
-
+<p class="intro">Olá, eu sou Leonardo Vasconcelos, um desenvolvedor de software.</p>
+<section class="perfil">   
+    <img src="https://avatars.githubusercontent.com/u/70069239?v=4" class="avatar"/>
+    <p>
+        <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/javascript/javascript-plain.svg" height="40" width="40" style="max-width:100%;" title="JavaScript">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/php/php-plain.svg" height="40" width="40" style="max-width:100%;" title="PHP">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/laravel/laravel-plain.svg" height="40" width="40" style="max-width:100%;" title="Laravel">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/python/python-plain.svg" height="40" width="40" style="max-width:100%;" title="Python">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/mysql/mysql-original.svg" height="40" width="40" style="max-width:100%;" title="SQL">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/Vegas_Pro_15.0.png" height="40" width="40" style="max-width:100%;" title="Vegas Pro">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/photoshop/photoshop-line.svg" height="40" width="40" style="max-width:100%;" title="Photoshop">
+    </p>
 </section>
- 
+
+<section class="posts"     background-color="#f4f4f4">
+
+<h1>Teste</h1>
+<h1>Teste</h1>
+<h1>Teste</h1>
+<h1>Teste</h1>
+<h1>Teste</h1>
+<h1>Teste</h1>
+</section>
 
 @endsection
