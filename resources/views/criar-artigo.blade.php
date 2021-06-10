40cdd1fa20ae4eba84c2b8b21c1d9bd1d724ad9a
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-10 15:00:47 -0300

    Estilizando tela do autor

--- resources/views/criar-artigo.blade.php
+++ resources/views/criar-artigo.blade.php
@@ -1,7 +1,13 @@
+@extends('layout.layout_user')
+@section('topBar')
+
+@section('content')
+<link rel="stylesheet" href="/css/buttonReset.css">
+
 <form action="ler.php" method="post">
     <textarea name="txtConteudo" id="txtConteudo"></textarea>
     <input type="submit" value="Enviar"/>
 </form>
-
 <script src="/vendor/ckeditor5/build/ckeditor.js"></script>
 <script src="/js/ckeditor5.js"> </script>
+@endsection
