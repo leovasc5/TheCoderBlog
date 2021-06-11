f788ff6f6de21e3bcf13883054c2c3cbbc8439be
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-11 08:01:16 -0300

    Estrutura do blade para o add-video criada

--- resources/views/add-video.blade.php
+++ resources/views/add-video.blade.php
@@ -3,5 +3,12 @@
 
 @section('content')
 <link rel="stylesheet" href="/css/buttonReset.css">
+<form action="valida-video" method="post">
+    @csrf
+    <input type="text" name="titulo"/><br>
+    <input type="text" name="url"/><br>
+    <textarea name="descricao" cols="30" rows="10"></textarea><br>
+    <input type="submit" value="Adicionar">
+</form>
 
 @endsection
