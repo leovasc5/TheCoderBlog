e1c26737d4a7d1eea6993efa0eaad8eebb0b4ee0
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-11 08:50:50 -0300

    Criado Models e Migration da aba 'projetos'

---
+++ database/migrations/2021_06_11_114917_create_projetos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjetosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projetos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('conteudo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projetos');
+    }
+}
