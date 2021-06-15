a9078e5bf291618d1351b2227061087395451a94
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-15 15:43:21 -0300

    Criado campo de comentários na tela de artiso

--- resources/views/artigo.blade.php
+++ resources/views/artigo.blade.php
@@ -13,4 +13,14 @@
 {!! $artigo->conteudo !!}
 </div>
 
+<div class="campoComentarios">
+    <h2>Comentários</h2>
+    <form action="/valida_comentario_artigo" method="post">
+        @csrf
+        <input type="text" placeholder="Username">
+        <br>
+        <textarea placeholder="Digite aqui o seu comentário" cols="80" rows="10"></textarea>
+    </form>
+</div>
+
 @endsection
