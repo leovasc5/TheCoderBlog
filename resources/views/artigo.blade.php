064242bcf1b59e69d60b3697ccfea33cfb564c9d
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-15 14:58:02 -0300

    Iniciando tratamento de artigos individuais

--- resources/views/artigo.blade.php
+++ resources/views/artigo.blade.php
@@ -1,9 +1,14 @@
 @extends('layout.layout')
 @section('title', 'TheCoderBlog - Vídeos')
-<link rel="stylesheet" href="/css/buttonReset.css">
 
 @section('topBar')
 
 @section('content')
+<link rel="stylesheet" href="/css/buttonReset.css">
+<link rel="stylesheet" href="/css/artigo.css">
+
+<div class="box_artigo">
+{!! $artigo->conteudo !!}
+</div>
 
 @endsection
