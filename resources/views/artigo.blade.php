bdb8d98b14c5f70c6c4e668717458392dc654f20
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-16 11:01:37 -0300

    Fim de estilização -> Indo para a parte de comentários do projeto

--- resources/views/artigo.blade.php
+++ resources/views/artigo.blade.php
@@ -9,7 +9,48 @@
 
 <div class="box_artigo">
     <label><b>Autor:</b> Leonardo Vasconcelos Paulino</label>
-    <label><b>Data:</b> {{$artigo->created_at}}
+    <label class="fr"><b>Data:</b> @php
+        $array_time = explode(' ', $artigo->created_at);
+        $array_dia = explode('-', $array_time[0]);
+
+        echo $array_dia[2].' de ';
+        if($array_dia[1] == '01'){
+          echo 'Janeiro';
+        }
+        if($array_dia[1] == '02'){
+          echo 'Fevereiro';
+        }
+        if($array_dia[1] == '03'){
+          echo 'Março';
+        }
+        if($array_dia[1] == '04'){
+          echo 'Abril';
+        }
+        if($array_dia[1] == '05'){
+          echo 'Maio';
+        }
+        if($array_dia[1] == '06'){
+          echo 'Junho';
+        }
+        if($array_dia[1] == '07'){
+          echo 'Julho';
+        }
+        if($array_dia[1] == '08'){
+          echo 'Agosto';
+        }
+        if($array_dia[1] == '09'){
+          echo 'Setembro';
+        }
+        if($array_dia[1] == '10'){
+          echo 'Outubro';
+        }
+        if($array_dia[1] == '11'){
+          echo 'Novembro';
+        }if($array_dia[1] == '12'){
+          echo 'Dezembro';
+        }
+        echo " de $array_dia[0]";
+      @endphp</label>
 {!! $artigo->conteudo !!}
 </div>
 
@@ -26,12 +67,12 @@
         <br>
         <textarea placeholder="Digite aqui o seu comentário" name="conteudo" maxlength="191" cols="80" rows="10"></textarea>
         <br>
-        <input type="submit" value="Comentar"/>
+        <input type="submit" value="Comentar" class="box_submit"/>
     </form>
 
     @foreach($comentarios as $comentario)
         <div class="comment">
-            <label>{{$comentario->username}}</label>
+            <p><b>{{$comentario->username}}</b></p>
             <p>{{$comentario->conteudo}}</p>
         </div>
     @endforeach
