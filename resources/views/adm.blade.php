3a66372e7ba9aeb7aa96c423b41fb3fd5de7e854
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-16 15:41:39 -0300

    Final do dia. Estilizado a tela de administrar posts

--- resources/views/adm.blade.php
+++ resources/views/adm.blade.php
@@ -3,6 +3,27 @@
 
 @section('content')
 <link rel="stylesheet" href="/css/buttonReset.css">
+<link rel="stylesheet" href="/css/buttonAdmin.css">
+
+<center>
+<div class="center">
+    <a href="/editar-artigo">
+        <input type="submit" class="button" value="Editar Artigo"/>
+    </a>
+</div>
+
+<div class="center">
+    <a href="/editar-video">
+        <input type="submit" class="button" value="Editar Vídeo"/>
+    </a>
+</div>
+
+<div class="center">
+    <a href="/editar-projeto">
+        <input type="submit" class="button" value="Editar Projeto"/>
+    </a>
+</div>
+</center>
 
 
 @endsection
